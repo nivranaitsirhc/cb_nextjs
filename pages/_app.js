d8c49d356a8d6637b09f5f77c192973477e547e7
Christian Arvin Catajoy Cabo <cacc.greenleaflabworks@gmail.com>
2021-03-06 12:59:34 +0800

    surpress re-hydration warnings on client side

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 
 function App({ Component, pageProps }) {
 	return (
-		<div>
+		<div suppressHydrationWarning>
 			{
 				// Prevent Server from rendering React Router
 				typeof window === 'undefined' ? null : <Component {...pageProps} />
